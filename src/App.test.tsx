b23a529bf10a5a058b9b52df28b9b3f20fe6c989
fdgbatarse <fgonzalez@applaudostudios.com>
2022-10-21 18:36:27 -0600

    ✅ Add app render test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import { describe } from 'vitest'
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
 
-describe('App', () => { 
-  it.todo('should render');
+import App from "./App";
+
+describe("App", () => {
+  it("should render", () => {
+    render(<App />);
+  });
   it.todo('should have title "Mi DUI es valido?"');
-  it.todo('should have an input text');
+  it.todo("should have an input text");
   it.todo('should have a button with text "Validar"');
   /* Lo que demas que se les ocurra que puedan probar */
-})
+});
