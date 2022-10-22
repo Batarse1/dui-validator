90c7348877a0039624d99557964f60a675b4cffd
Batarse1 <00014419@uca.edu.sv>
2022-10-21 20:52:20 -0600

    🧪 Add App failing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import App from "./App";
@@ -29,5 +29,27 @@ describe("App", () => {
     const { getByRole } = render(<App />);
     const button = getByRole("button", { name: "Validar" });
     userEvent.click(button);
+  });
+  // it should display Es valido! when input is 061921617
+  it('should display "Es valido!" when input is 061921617', async () => {
+    const { getByRole, getByText } = render(<App />);
+    const input = getByRole("textbox");
+    const button = getByRole("button", { name: "Validar" });
+    userEvent.type(input, "061921617");
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(getByText("Es valido!")).toBeInTheDocument();
+    });
+  });
+  // it should display No es valido! when input is 061921618
+  it('should display "No es valido!" when input is 061921618', async () => {
+    const { getByRole, getByText } = render(<App />);
+    const input = getByRole("textbox");
+    const button = getByRole("button", { name: "Validar" });
+    userEvent.type(input, "061921618");
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(getByText("No es valido!")).toBeInTheDocument();
+    });
   });
 });
